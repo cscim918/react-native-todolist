816a5fc93e94a2e19b7640383a016ea2b9d0f722
Frank <frank@bunnit.kr>
2021-10-29 12:00:13 +0900

    todolist add,delete,complete_check complete

---
+++ components/TodoInsert.js
@@ -0,0 +1,53 @@
+// 텍스트 입력창과 추가 버튼이 들어갈 부분
+import React, {useState} from 'react';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
+
+const TodoInsert = ({onAddTodo}) => {
+  const [newTodoItem, setNewTodoItem] = useState('');
+
+  const todoInputHandler = newTodo => {
+    setNewTodoItem(newTodo);
+  };
+
+  const addTodoHandler = () => {
+    onAddTodo(newTodoItem);
+    setNewTodoItem('');
+  };
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        placeholder="할 일 추가하기"
+        placeholderTextColor={'#999'}
+        onChangeText={todoInputHandler}
+        value={newTodoItem}
+        autoCorrect={false}
+      />
+      <View style={styles.button}>
+        <Button title={'추가'} onPress={addTodoHandler} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20,
+  },
+  button: {
+    marginRight: 10,
+  },
+});
+
+export default TodoInsert;
